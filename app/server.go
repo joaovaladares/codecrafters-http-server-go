507f29b54835ad4227a31a323b660f80e6f23d97
Joao Valadares <joaovvaladares1@gmail.com>
2024-08-04 22:31:53 -0300

    respond to request with 200 OK

--- app/server.go
+++ app/server.go
@@ -19,9 +19,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	// Respond to HTTP request with a 200 OK
+	fmt.Println("Server running on port 4221")
+
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
