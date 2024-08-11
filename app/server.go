93b760bfccbb2b5c20ddf862c82d849d30b0f249
Joao Valadares <joaovvaladares1@gmail.com>
2024-08-11 11:10:51 -0300

    Extract URL path

--- app/server.go
+++ app/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
@@ -25,8 +26,26 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Respond to HTTP request with a 200 OK
-	fmt.Println("Server running on port 4221")
+  // Read request buffer
+  buf := make([]byte, 1024)
+  n, err := conn.Read(buf)
+  if err != nil {
+    fmt.Println("Error reading request: ", err.Error())
+    os.Exit(1)
+  }
 
+  // Extract request URL path from request
+  urlPath := string(buf[:n])
+  fmt.Println("Request URL: ", urlPath)
+ 
+  // Respond with 404 Not Found if request path is not "GET / HTTP/1.1"
+  if !strings.HasPrefix(urlPath, "GET / HTTP/1.1") {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		return
+	}
+
+	// Respond to HTTP request with a 200 OK
 	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+
+  conn.Close()
 }
